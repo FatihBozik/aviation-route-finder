f113c20d4c4f99834fecf778a403f516e3e79a2c
Fatih Bozik <bozikfatih@gmail.com>
2025-03-24 22:49:25 +0300

    Initialize spring boot project

---
+++ src/main/java/com/fatihbozik/aviationroutefinder/AviationRouteFinderApplication.java
@@ -0,0 +1,12 @@
+package com.fatihbozik.aviationroutefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationRouteFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AviationRouteFinderApplication.class, args);
+    }
+}
